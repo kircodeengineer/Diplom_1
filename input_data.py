b784f5607de3a862df129365442617dc21e0abbe
Kir <kiruha1955@yandex.ru>
2025-03-02 18:39:08 +0300

    refactoring: Заменить database на моки в тестах burger

--- input_data.py
+++ input_data.py
@@ -1,3 +1,11 @@
 class BunData:
     NAME = "Тестовая булка"
-    PRICE = 123
+    PRICE = 100
+
+class FirstIngredientData:
+    NAME = "Первый тестовый ингредиент"
+    PRICE = 200
+
+class SecondIngredientData:
+    NAME = "Второй тестовый ингредиент"
+    PRICE = 300
--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,10 +1,9 @@
 from unittest.mock import Mock
 
-from database import Database
 from praktikum.burger import Burger, Bun
 from praktikum.ingredient_types import INGREDIENT_TYPE_FILLING, INGREDIENT_TYPE_SAUCE
 
-from input_data import BunData
+from input_data import BunData, FirstIngredientData, SecondIngredientData
 
 
 class TestBurger:
@@ -17,13 +16,13 @@ class TestBurger:
     def test_add_ingredient(self):
         burger = Burger()
         mock_ingredient = Mock()
-        mock_ingredient.get_name.return_value = BunData.NAME
-        mock_ingredient.get_price.return_value = BunData.PRICE
-        mock_ingredient.get_type.return_value = INGREDIENT_TYPE_FILLING
+        mock_ingredient.name = FirstIngredientData.NAME
+        mock_ingredient.price = FirstIngredientData.PRICE
+        mock_ingredient.type = INGREDIENT_TYPE_SAUCE
         burger.add_ingredient(mock_ingredient)
-        assert burger.ingredients[0].get_name() == BunData.NAME
-        assert burger.ingredients[0].get_price() == BunData.PRICE
-        assert burger.ingredients[0].get_type() == INGREDIENT_TYPE_FILLING
+        assert burger.ingredients[0].name == mock_ingredient.name
+        assert burger.ingredients[0].price == mock_ingredient.price
+        assert burger.ingredients[0].type == mock_ingredient.type
 
     def test_remove_ingredient(self):
         burger = Burger()
@@ -35,55 +34,90 @@ class TestBurger:
 
     def test_move_ingredient(self):
         burger = Burger()
-        database = Database()
-        buns = database.available_buns()[0]
-        sauce = database.available_ingredients()[0]
-        filling = database.available_ingredients()[3]
-        burger.set_buns(buns)
-        burger.add_ingredient(sauce)
+
+        mock_buns = Bun(BunData.NAME, BunData.PRICE)
+
+        mock_first_ingredient = Mock()
+        mock_first_ingredient.name = FirstIngredientData.NAME
+        mock_first_ingredient.get_name.return_value = FirstIngredientData.NAME
+        mock_first_ingredient.get_price.return_value = FirstIngredientData.PRICE
+        mock_first_ingredient.get_type.return_value = INGREDIENT_TYPE_SAUCE
+
+        mock_second_ingredient = Mock()
+        mock_second_ingredient.name = SecondIngredientData.NAME
+        mock_second_ingredient.get_name.return_value = SecondIngredientData.NAME
+        mock_second_ingredient.get_price.return_value = SecondIngredientData.PRICE
+        mock_second_ingredient.get_type.return_value = INGREDIENT_TYPE_FILLING
+
+        burger.set_buns(mock_buns)
+        burger.add_ingredient(mock_first_ingredient)
         sauce_index = len(burger.ingredients) - 1
-        burger.add_ingredient(filling)
+        burger.add_ingredient(mock_second_ingredient)
         filling_index = len(burger.ingredients) - 1
         burger.move_ingredient(sauce_index, filling_index)
-        expected_price = buns.price * 2
-        expected_price += sauce.price
-        expected_price += filling.price
-        expected_receipt = f"(==== {buns.name} ====)\n" \
-                           f"= filling {filling.name} =\n" \
-                           f"= sauce {sauce.name} =\n" \
-                           f"(==== {buns.name} ====)\n\n" \
+        expected_price = mock_buns.price * 2
+        expected_price += mock_first_ingredient.get_price()
+        expected_price += mock_second_ingredient.get_price()
+        expected_receipt = f"(==== {mock_buns.name} ====)\n" \
+                           f"= filling {mock_second_ingredient.name} =\n" \
+                           f"= sauce {mock_first_ingredient.name} =\n" \
+                           f"(==== {mock_buns.name} ====)\n\n" \
                            f"Price: {expected_price}"
         assert burger.get_receipt() == expected_receipt
 
     def test_get_price(self):
         burger = Burger()
-        database = Database()
-        buns = database.available_buns()[0]
-        sauce = database.available_ingredients()[0]
-        filling = database.available_ingredients()[3]
-        burger.set_buns(buns)
-        burger.add_ingredient(sauce)
-        burger.add_ingredient(filling)
-        expected_price = buns.price * 2
-        expected_price += sauce.price
-        expected_price += filling.price
+
+        mock_buns = Bun(BunData.NAME, BunData.PRICE)
+
+        mock_first_ingredient = Mock()
+        mock_first_ingredient.name = FirstIngredientData.NAME
+        mock_first_ingredient.get_name.return_value = FirstIngredientData.NAME
+        mock_first_ingredient.get_price.return_value = FirstIngredientData.PRICE
+        mock_first_ingredient.get_type.return_value = INGREDIENT_TYPE_SAUCE
+
+        mock_second_ingredient = Mock()
+        mock_second_ingredient.name = SecondIngredientData.NAME
+        mock_second_ingredient.get_name.return_value = SecondIngredientData.NAME
+        mock_second_ingredient.get_price.return_value = SecondIngredientData.PRICE
+        mock_second_ingredient.get_type.return_value = INGREDIENT_TYPE_FILLING
+
+        burger.set_buns(mock_buns)
+        burger.add_ingredient(mock_first_ingredient)
+        burger.add_ingredient(mock_second_ingredient)
+
+        expected_price = mock_buns.price * 2
+        expected_price += mock_first_ingredient.get_price()
+        expected_price += mock_second_ingredient.get_price()
         assert burger.get_price() == expected_price
 
     def test_get_receipt(self):
         burger = Burger()
-        database = Database()
-        buns = database.available_buns()[0]
-        sauce = database.available_ingredients()[0]
-        filling = database.available_ingredients()[3]
-        burger.set_buns(buns)
-        burger.add_ingredient(sauce)
-        burger.add_ingredient(filling)
-        expected_price = buns.price * 2
-        expected_price += sauce.price
-        expected_price += filling.price
-        expected_receipt = f"(==== {buns.name} ====)\n"\
-                           f"= sauce {sauce.name} =\n"\
-                           f"= filling {filling.name} =\n"\
-                           f"(==== {buns.name} ====)\n\n"\
+
+        mock_buns = Bun(BunData.NAME, BunData.PRICE)
+
+        mock_first_ingredient = Mock()
+        mock_first_ingredient.name = FirstIngredientData.NAME
+        mock_first_ingredient.get_name.return_value = FirstIngredientData.NAME
+        mock_first_ingredient.get_price.return_value = FirstIngredientData.PRICE
+        mock_first_ingredient.get_type.return_value = INGREDIENT_TYPE_SAUCE
+
+        mock_second_ingredient = Mock()
+        mock_second_ingredient.name = SecondIngredientData.NAME
+        mock_second_ingredient.get_name.return_value = SecondIngredientData.NAME
+        mock_second_ingredient.get_price.return_value = SecondIngredientData.PRICE
+        mock_second_ingredient.get_type.return_value = INGREDIENT_TYPE_FILLING
+
+        burger.set_buns(mock_buns)
+        burger.add_ingredient(mock_first_ingredient)
+        burger.add_ingredient(mock_second_ingredient)
+
+        expected_price = mock_buns.price * 2
+        expected_price += mock_first_ingredient.get_price()
+        expected_price += mock_second_ingredient.get_price()
+        expected_receipt = f"(==== {mock_buns.name} ====)\n" \
+                           f"= sauce {mock_first_ingredient.name} =\n" \
+                           f"= filling {mock_second_ingredient.name} =\n" \
+                           f"(==== {mock_buns.name} ====)\n\n" \
                            f"Price: {expected_price}"
         assert burger.get_receipt() == expected_receipt
